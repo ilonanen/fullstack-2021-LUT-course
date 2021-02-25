431dc40fcbb308e0b96f52ee3f313da96c0c001d
Ilona Engblom <ilona.engblom@tuta.io>
2021-02-25 15:41:32 +0200

    Added a get('*') to the server app.js file

--- coursework/meanauthapp/app.js
+++ coursework/meanauthapp/app.js
@@ -45,6 +45,10 @@ app.get('/', (req, res) => {
     res.send('Invalid endpoint')
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
